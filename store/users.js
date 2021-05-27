51c1994be073043fe2803d7a9a299bbe2104305b
Alireza Sharifpour <your_email_address@example.com>
2021-05-27 18:16:41 +0430

    After removing Promises

--- store/users.js
+++ store/users.js
@@ -1,23 +1,23 @@
 export const state = () => ({
-  users: {}
+  users: []
 });
 
 export const mutations = {
-  setUsers(state, users) {
+  setUsers(state) {
     state.status = "loading";
   }
 };
 
 export const actions = {
-  async getUsers({ commit }, payload) {
+  async getUsers({ commit }) {
     try {
       const users = await this.$axios.get(
         "https://jsonplaceholder.typicode.com/users"
       );
       commit("setUsers", users);
-      return Promise.resolve(users);
+      return users;
     } catch (error) {
-      return Promise.reject(error);
+      return error;
     }
   }
 };
